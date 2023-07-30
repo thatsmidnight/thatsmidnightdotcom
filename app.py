1f8b51101dae9b040e408779f9aa674e27cf0c1f
thatsmidnight <10793854+thatsmidnight@users.noreply.github.com>
2023-07-29 17:08:22 -0700

    Add missing stack_name parameter

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from aws_cdk import App
 from cdk.stacks import MyStaticSiteStack
 
 app = App()
-stack = MyStaticSiteStack(app, "my-static-website-stack")
+
+stack_name = "my-static-website-stack"
+stack = MyStaticSiteStack(app, stack_name, stack_name=stack_name)
 
 app.synth()
